95b4b1de851bb6fd44900b0169a75309fe9ae539
aJesusCode <ajesusnode@gmail.com>
2023-06-09 16:07:20 +0200

    experimental summarizator on a given theme, reusable url fetch function, error handling

--- src/app/api/experiment/route.ts
+++ src/app/api/experiment/route.ts
@@ -7,7 +7,10 @@ import { HumanChatMessage, AIChatMessage } from "langchain/schema";
 import { PromptTemplate } from "langchain/prompts";
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
 import { LLMChain } from "langchain/chains";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import axios from "axios";
+import { serperSearch } from "@/app/utils/serperSearch";
 
 export async function POST(req: NextRequest) {
     try {
@@ -25,37 +28,46 @@ export async function POST(req: NextRequest) {
         });
 
         //query search function utilizing serper.dev
-        async function getSearchResult() {
-            let data = JSON.stringify({
-                q: body.query,
-            });
-            let config = {
-                method: "post",
-                url: "https://google.serper.dev/search",
-                headers: {
-                    "X-API-KEY": "72c4402a87d287368aeb1b0210575ab0b1e76404",
-                    "Content-Type": "application/json",
-                },
-                data: data,
-            };
-            //reciving a response
-            const response = await axios(config)
-                .then((res) => {
-                    console.log("extracted");
-                    return res.data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-            //we need to make a response as a string to use it in Langchain
-            return JSON.stringify(response);
-        }
-        //calling intilized function
-        const response = await getSearchResult();
+        // async function getSearchResult() {
+        //     try {
+        //         let data = JSON.stringify({
+        //             q: body.query,
+        //         });
+        //         let config = {
+        //             method: "post",
+        //             url: "https://google.serper.dev/search",
+        //             headers: {
+        //                 "X-API-KEY": "72c4402a87d287368aeb1b0210575ab0b1e76404",
+        //                 "Content-Type": "application/json",
+        //             },
+        //             data: data,
+        //         };
 
+        //         const response = await axios(config);
+        //         return JSON.stringify(response.data);
+        //     } catch (error) {
+        //         console.log("Axios Error:", error);
+
+        //         // Generate an appropriate error response
+        //         let errorMessage = "An error occurred";
+        //         let statusCode = 500;
+
+        //         // Check if the error response contains specific information
+        //         if (error.response) {
+        //             errorMessage = error.response.data.message;
+        //             statusCode = error.response.status;
+        //         }
+
+        //         // Return the error response to the client
+        //         return JSON.stringify({ errorMessage, statusCode });
+        //     }
+        // }
+
+        //calling intilized function
+        const response = await serperSearch(body.query);
         //defining template with Langchain
         const template =
-            "Extract urls into a list from {response}. Above is a search result for a {query}";
+            "Extract urls into a numbered list format from {response}. Above is a search result for a {query}";
         //defining prompt Template with variables. We have response from above function call and query from client
         const promptTemplate = new PromptTemplate({
             template: template,
@@ -72,12 +84,92 @@ export async function POST(req: NextRequest) {
             response: response,
             query: query,
         });
-        //sending list of urls to a client
 
-        //TODO: extract text from each link
-        //TODO:create summaries for each extrated data
-        return NextResponse.json(urls);
+        // extract text from each link
+        const processUrls = async () => {
+            try {
+                // Creating an array from the URLs string
+                const urlsArray = urls.split("\n").map((url) => url.trim());
+                // Defining an empty array
+                const results = [];
+                // Running a loop on each link from the array
+                for (const urlArr of urlsArray) {
+                    try {
+                        // Utilizing Cheerio loader from Langchain
+                        const loader = new CheerioWebBaseLoader(
+                            urlArr.substring(3),
+                            {
+                                // Selecting only p tags (assuming that all the content is under this selector)
+                                selector: "p",
+                            }
+                        );
+                        const docs = await loader.load();
+                        results.push(docs);
+                    } catch (error) {
+                        // Handle the fetch error for a specific URL
+                        console.error(`Error fetching URL: ${urlArr}`, error);
+                        //continue with the loop
+                        results.push(null); // Add a placeholder value or handle the error as needed
+                    }
+                }
+                return results;
+            } catch (error) {
+                // Handle any other potential errors
+                console.error("An error occurred during URL processing", error);
+                return [];
+            }
+        };
+
+        const extractedHtml = await processUrls();
+
+        //split text into small chunks
+        const smallChunks = async () => {
+            const results = [];
+            for (const doc of extractedHtml) {
+                const splitter = new RecursiveCharacterTextSplitter({
+                    chunkSize: 3000,
+                    chunkOverlap: 200,
+                });
+                const output = await splitter.splitDocuments(doc);
+                results.push(output);
+            }
+
+            return results;
+        };
+
+        const chunks = await smallChunks();
+
+        //create summaries for each extrated data
+        const ideas = async () => {
+            let ideas = [];
+            const ideasTemplate =
+                "You are a world class experienced investor. Analyze this information: {chunks}. Then follow this rules: Create one sentence summary.";
+            const ideasPromptTemplate = new PromptTemplate({
+                template: ideasTemplate,
+                inputVariables: ["chunks"],
+            });
+            const ideasChain = new LLMChain({
+                llm: model,
+                prompt: ideasPromptTemplate,
+                verbose: true,
+            });
+            for (const idea of chunks) {
+                if (idea && idea.length && idea[0].pageContent) {
+                    const pageContent = idea[0].pageContent;
+                    const res = await ideasChain.predict({
+                        chunks: pageContent,
+                    });
+                    ideas.push(res);
+                }
+            }
+            return ideas;
+        };
+
+        const idea = await ideas();
+
+        return NextResponse.json(idea);
     } catch (err) {
-        throw new Error(err);
+        console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", err);
+        return NextResponse.json({ error: `Error: ${err}` }, { status: 500 });
     }
 }
--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function ExperimentPage() {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState([]);
     const [query, setQuery] = useState("");
     const [chatHistory, setChatHistory] = useState([]);
+    const [error, setError] = useState(null);
 
     async function getExperiments() {
         const res = await fetch("api/experiment", {
@@ -13,23 +14,48 @@ export default function ExperimentPage() {
             body: JSON.stringify({ query }),
         });
         const data = await res.json();
-        console.log(data);
-        setResult(data);
+        if (data.error) {
+            setError(data.error);
+        } else {
+            console.log(data);
+            setResult(data);
+        }
     }
 
-    const lists = result.split("\n").map((list, index) => (
-        <Link
-            key={index}
-            className="mb-2 flex text-secondary"
-            href={list.substring(3)}
-        >
-            {list}
-        </Link>
-    ));
-    console.log(lists);
+    // const lists = result.split("\n").map((list, index) => (
+    //     <Link
+    //         key={index}
+    //         className="mb-2 flex text-secondary"
+    //         href={list.substring(3)}
+    //     >
+    //         {list}
+    //     </Link>
+    // ));
+    console.log(result);
 
     return (
         <div className="flex flex-col justify-center">
+            {error && (
+                <div
+                    className="alert alert-error"
+                    onClick={() => setError(null)}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="stroke-current shrink-0 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                    <span>Error! {error}</span>
+                </div>
+            )}
             <h2 className="text-2xl font-black ">
                 This is experimental Page for testing Langchain Approaches
             </h2>
@@ -44,11 +70,19 @@ export default function ExperimentPage() {
             >
                 SEND
             </button>
-            {result && (
-                <div className="flex flex-col bg-base-300 pt-8 rounded-md px-4">
-                    {lists}
-                </div>
-            )}
+            <div>
+                {result && (
+                    <div className="flex flex-col bg-base-300 pt-8 rounded-md px-4"></div>
+                )}
+                {result.map((res, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-col bg-base-300 pt-8 rounded-md px-4"
+                    >
+                        {res}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
